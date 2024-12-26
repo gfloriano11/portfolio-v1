12b22ec9f102d8627d9d2b25de5505d65b5dc0d8
gfloriano11 <gflorianodev@gmail.com>
2024-12-26 12:16:22 -0300

    feat: make the connection (html to js)

--- src/frontend/script/menu.js
+++ src/frontend/script/menu.js
@@ -2,5 +2,5 @@ const menuIcon = document.querySelector('#menu_icon');
 const menu = document.querySelector('#teste');
 
 menuIcon.addEventListener('click', () => {
-    
+    console.log('abri o menu')
 });
