68a74a3f4e9b9499e5945e7d3e11a8489d157651
gfloriano11 <gflorianodev@gmail.com>
2024-12-27 20:46:15 -0300

    feat: animation to the menu icon

--- src/frontend/script/menu.js
+++ src/frontend/script/menu.js
@@ -1,12 +1,29 @@
 const menuIcon = document.querySelector('#menu_icon');
 const menu = document.querySelector('.menu_options');
+const removeBar = document.querySelector('.remove_bar');
+const leftBar = document.querySelector('.left_bar');
+const rightBar = document.querySelector('.right_bar')
+
+menu.style.transform = 'translateY(-450px)';
 
 menuIcon.addEventListener('click', () => {
 
-    if(menu.classList == 'menu_options hide'){
-        menu.classList.remove('hide')
+
+    if(menu.style.transform == 'translateY(-450px)'){
+
+        menu.style.transform = 'translateY(0)';
+        removeBar.style.opacity = '0%';
+
+        leftBar.style.transform = 'rotate(45deg) translateY(5px) translateX(7px)';
+
+        rightBar.style.transform = 'rotate(-45deg) translateY(-5px) translateX(7px)';
+        
     } else {
-        menu.classList.add('hide');
+
+        menu.style.transform = 'translateY(-450px)';
+        removeBar.style.opacity = '100%';
+        leftBar.style.transform = 'rotate(0)';
+        rightBar.style.transform = 'rotate(0)';
     }
     
 });
