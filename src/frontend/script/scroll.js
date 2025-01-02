af2859e64a36354cf2a3158023237636f6c82ccc
gfloriano11 <gflorianodev@gmail.com>
2025-01-02 10:56:10 -0300

    feat: add animation to all menu [desktop and mobile]

--- src/frontend/script/scroll.js
+++ src/frontend/script/scroll.js
@@ -1,19 +1,54 @@
 const startButton = document.querySelector('#start_button');
-const homeButton = document.querySelector('#home');
-const skillsButton = document.querySelector('#skill');
+const homeButton = document.querySelectorAll('#home');
+const skillsButton = document.querySelectorAll('#skill');
+const projectsButton = document.querySelectorAll('#project');
+const contactButton = document.querySelectorAll('#contact');
 
 const start = document.querySelector('#top_space');
-const skills = document.querySelector('#skills_container')
+const skills = document.querySelector('#skills_container');
+const projects = document.querySelector('#projects_container');
+const contactMe = document.querySelector('#contacts_container');
 
 startButton.addEventListener('click', scrollTop);
-homeButton.addEventListener('click', scrollTop);
 
-skillsButton.addEventListener('click', scrollSkills);
+homeButton.forEach((home) => {
+
+    home.addEventListener('click', scrollTop);
+});
+
+skillsButton.forEach((skills) => {
+
+    skills.addEventListener('click', scrollSkills);
+});
+
+projectsButton.forEach((projects) => {
+
+    projects.addEventListener('click', scrollProjects);
+
+});
+
+contactButton.forEach((contact) => {
+
+
+    contact.addEventListener('click', scrollContact);
+});
+
+
+
 
 function scrollTop(){
-    start.scrollIntoView({behavior: 'smooth'});
+    start.scrollIntoView({behavior: 'smooth', block: 'center'});
 }
 
 function scrollSkills(){
-    skills.scrollIntoView({behavior: 'smooth'})
+    console.log('kk')
+    skills.scrollIntoView({behavior: 'smooth', block: 'center'})
+}
+
+function scrollProjects(){
+    projects.scrollIntoView({behavior: 'smooth', block: 'center'});
+}
+
+function scrollContact(){
+    contactMe.scrollIntoView({behavior: 'smooth', block: 'center'});
 }
