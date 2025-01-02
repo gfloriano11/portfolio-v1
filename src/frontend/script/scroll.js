c5fb6ff327b61f6699a1d4ed21d093c7773e4090
gfloriano11 <gflorianodev@gmail.com>
2025-01-01 23:05:17 -0300

    feat: add scroll animation

--- src/frontend/script/scroll.js
+++ src/frontend/script/scroll.js
@@ -1,9 +1,19 @@
 const startButton = document.querySelector('#start_button');
+const homeButton = document.querySelector('#home');
+const skillsButton = document.querySelector('#skill');
+
 const start = document.querySelector('#top_space');
+const skills = document.querySelector('#skills_container')
 
 startButton.addEventListener('click', scrollTop);
+homeButton.addEventListener('click', scrollTop);
+
+skillsButton.addEventListener('click', scrollSkills);
 
 function scrollTop(){
-    console.log('hello!');
-    start.scrollIntoView({ behavior: 'smooth'});
+    start.scrollIntoView({behavior: 'smooth'});
+}
+
+function scrollSkills(){
+    skills.scrollIntoView({behavior: 'smooth'})
 }
